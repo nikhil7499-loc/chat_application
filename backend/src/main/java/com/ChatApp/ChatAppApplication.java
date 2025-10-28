a88ca222ff90145b46bcf141938c1c8eed875d00
nikhilgaikwad <nikhilgaikwadmh10@gmail.com>
2025-10-28 08:53:55 +0530

    changes

--- backend/src/main/java/com/ChatApp/ChatAppApplication.java
+++ backend/src/main/java/com/ChatApp/ChatAppApplication.java
@@ -1,4 +1,4 @@
-package com.ChatApp.ChatApp;
+package com.ChatApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ backend/src/main/java/com/ChatApp/Entities/User.java
@@ -0,0 +1,23 @@
+package com.ChatApp.Entities;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+
+
+
+@Entity
+public class User {
+    private UUID Id;
+    private String username;
+    private String password;
+    private String email;
+    private Date date_of_birth;
+    private String gender;
+    private String profile_picture;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+
+    
+}
