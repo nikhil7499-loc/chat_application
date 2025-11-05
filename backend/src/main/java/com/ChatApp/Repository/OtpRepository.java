cdb1433be67ffbcd95a13c953ea0513b093a85f2
nikhilgaikwad <nikhilgaikwadmh10@gmail.com>
2025-11-05 08:59:51 +0530

    repo 2

--- backend/src/main/java/com/ChatApp/Repository/OtpRepository.java
+++ backend/src/main/java/com/ChatApp/Repository/OtpRepository.java
@@ -5,5 +5,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OtpRepository extends JpaRepository<Otp, String> {
-       
+    Optional<Otp> findTopByUserAndIsUsedFalseOrderByCreatedAtDesc(User user);
+    Optional<Otp> findByCodeAndUser(String code, User user);
+    boolean existsByUserAndIsUsedFalse(User user);
+    void deleteByExpiresAtBefore(Instant cutoff);
+    void deleteByUser(User user);
 }
