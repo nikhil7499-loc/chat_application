656027eb40718af0da157216917f58f412c885b6
nikhilgaikwad <nikhilgaikwadmh10@gmail.com>
2025-11-29 09:25:44 +0530

    c

--- backend/src/main/java/com/ChatApp/Config/SecurityConfig.java
+++ backend/src/main/java/com/ChatApp/Config/SecurityConfig.java
@@ -1,3 +1,33 @@
+@Configuration
+@EnablewebSecurity
+
 public class SecurityConfig {
+    @Value("${app.cors.allowed-original}")
+    private String[] allowedOrigins;
+
+    private final JwtAuthenticationfilter jwtAuthenticationfilter;
+
+    public SecurityConfig(JwtAuthenticationfilter jwtAuthenticationfilter){
+        this.jwtAuthenticationFilter=jwtAuthenticationFilter;
+    }
     
+    @Bean
+    public SedcuritryFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+        .csrf(csrf->csrf.disable())
+        .cors(Customizer.withDefaults())
+        .addFilterBefore(jwtAuthenticationFilter,UsernamePasswordAuthenticationFilter.class)
+        .authorizeHttpRequests(auth->auth.requestMatchers("/**").permitAll());
+        
+        return http.build();
+    }
+
+    @Bean public WebMyConfigurer corConfigurer(){
+        return new WebMvcConfigurer(){
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                regidtry.addMapping("/**").allowedOrigins(allowedorigins).allowedMethod("GET","POST","PUT","DELETE","OPTIONS","PATCH").allowedHeaders("*").alloweCredentials(true);
+            }
+        }
+    }
 }
