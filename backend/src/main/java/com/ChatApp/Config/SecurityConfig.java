5f0e0f72bccc6ff1a45ab6ea3793df71f3b670e3
nikhilgaikwad <nikhilgaikwadmh10@gmail.com>
2025-12-01 08:59:14 +0530

    updates

--- backend/src/main/java/com/ChatApp/Config/SecurityConfig.java
+++ backend/src/main/java/com/ChatApp/Config/SecurityConfig.java
@@ -1,43 +1,49 @@
 package com.ChatApp.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
-
-
-
+import com.ChatApp.Security.JwtAuthenticationFilter;
 
 
 
 @Configuration
-@EnablewebSecurity
-
+@EnableWebSecurity
 public class SecurityConfig {
     @Value("${app.cors.allowed-original}")
     private String[] allowedOrigins;
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    public SecurityConfig(JwtAuthenticationfilter jwtAuthenticationfilter){
-        this.jwtAuthenticationFilter=jwtAuthenticationFilter;
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationfilter){
+        this.jwtAuthenticationFilter=jwtAuthenticationfilter;
     }
     
     @Bean
-    public SedcuritryFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
         .csrf(csrf->csrf.disable())
-        .cors(Customizer.withDefaults())
+        .cors(org.springframework.security.config.Customizer.withDefaults())
         .addFilterBefore(jwtAuthenticationFilter,UsernamePasswordAuthenticationFilter.class)
         .authorizeHttpRequests(auth->auth.requestMatchers("/**").permitAll());
         
         return http.build();
     }
 
-    @Bean public WebMyConfigurer corConfigurer(){
+    @Bean 
+    public WebMvcConfigurer corsConfigurer(){
         return new WebMvcConfigurer(){
             @Override
             public void addCorsMappings(CorsRegistry registry){
-                regidtry.addMapping("/**").allowedOrigins(allowedorigins).allowedMethod("GET","POST","PUT","DELETE","OPTIONS","PATCH").allowedHeaders("*").alloweCredentials(true);
+                registry.addMapping("/**").allowedOrigins(allowedOrigins).allowedMethods("GET","POST","PUT","DELETE","OPTIONS","PATCH").allowedHeaders("*").allowCredentials(true);
             }
-        }
+        };
     }
 }
