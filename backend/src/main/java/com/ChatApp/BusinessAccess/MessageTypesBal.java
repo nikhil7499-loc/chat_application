71dd1e779091adb01f77a925ed9c1155009d028e
nikhilgaikwad <nikhilgaikwadmh10@gmail.com>
2026-01-16 11:35:38 +0530

    c

--- backend/src/main/java/com/ChatApp/BusinessAccess/MessageTypesBal.java
+++ backend/src/main/java/com/ChatApp/BusinessAccess/MessageTypesBal.java
@@ -2,6 +2,7 @@ package com.ChatApp.BusinessAccess.MessageTypeBal;
 import org.springframework.stereotype.Service;
 
 import com.ChatApp.DataAccess.MessageTypesDal;
+import com.ChatApp.Exceptions.ResourceNotFoundException;
 import com.ChatApp.Repository.MessageTypesRepository;
 
 import jakarta.transaction.Transactional;
@@ -13,5 +14,20 @@ public class MessageTypesBal {
 
     public MeassageTypesBal(MessageTypesDal messageTypesDal){
         this.messageTypesDal=messageTypesDal;
+    }
+
+    public List<MEssageTypes> getAllMessageTypes(){
+        return messageTypes.Dal.findAll();
+    }
+
+    public MessageType getMessageTypesId(short id){
+        return messageTypesDal.findById(id);
+    }
+
+    public MessageTyps getMessageTypesByName(String name){
+        if(name==null||name.isBlank()){
+            throw new ResourceNotFoundException("Message type name cannot be null or blank.");
+        }
+        return messageTypesDal.findByName(name.toLowerCase().trim());
     }
 }
