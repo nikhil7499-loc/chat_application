c12c80197b5157668b966db0a9d3b57ea043bf80
nikhilgaikwad <nikhilgaikwadmh10@gmail.com>
2026-01-16 11:12:49 +0530

    c

--- backend/src/main/java/com/ChatApp/DataAccess/MessageTypesDal.java
+++ backend/src/main/java/com/ChatApp/DataAccess/MessageTypesDal.java
@@ -1,8 +1,15 @@
 package com.ChatApp.DataAccess;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.ChatApp.Exceptions.DatabaseOperationException;
+import com.ChatApp.Entities.MessageTypes;
+import com.ChatApp.Exceptions.ResourceNotFoundException;
 import com.ChatApp.Repository.MessageTypesRepository;
+import com.mailjet.client.Resource;
+import com.mailjet.client.resource.Message;
 
 public class MessageTypesDal {
 
@@ -11,6 +18,30 @@ public class MessageTypesDal {
     @Autowired
     public MessageTypesDal(MessageTypesRepository _messageTypesRepository){
         this.messageTypesRepository = _messageTypesRepository;
+    }
+    public List<MessageTypes> findAll(){
+        try {
+            return messageTypesRepository.findAll();
+        } catch (Exception e) {
+            throw  new  DatabaseOperationException("Failed to fetch meassage types",e);
+        }
+    }
+
+    public MessageTypes findById(short id){
+        try {
+            return messageTypesRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Message type not found with ID: "+id));
+        } catch (Exception e) {
+            throw new DatabaseOperationException("Failed to fetch message type by ID",e);
+        }
+    }
+    public MessageTypes findByName(String name){
+        try {
+            return messageTypesRepository.findByName(name).orElseThrow(()-> new ResourceNotFoundException("Message type not found with name: "+name));
+        } catch (ResourceNotFoundException e) {
+            throw e;
+        }catch(Exception e){
+            throw new DatabaseOperationException("failed to fetch message type by name",e);
+        }
     }
 
 
