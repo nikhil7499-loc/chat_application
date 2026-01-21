beadb7c497b4e07999706756ae2c2e8e38b0c59d
nikhilgaikwad <nikhilgaikwadmh10@gmail.com>
2026-01-21 09:37:13 +0530

    message types controller initialized

--- backend/src/main/java/com/ChatApp/BusinessAccess/MessageTypesBal.java
+++ backend/src/main/java/com/ChatApp/BusinessAccess/MessageTypesBal.java
@@ -1,30 +1,34 @@
 package com.ChatApp.BusinessAccess.MessageTypeBal;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ChatApp.DataAccess.MessageTypesDal;
+import com.ChatApp.Entities.MessageTypes;
 import com.ChatApp.Exceptions.ResourceNotFoundException;
-import com.ChatApp.Repository.MessageTypesRepository;
-
-import jakarta.transaction.Transactional;
 
 @Service
 @Transactional(readOnly=true)
 public class MessageTypesBal {
     private final MessageTypesDal messageTypesDal;
 
-    public MeassageTypesBal(MessageTypesDal messageTypesDal){
-        this.messageTypesDal=messageTypesDal;
+    @Autowired
+    public MessageTypesBal(MessageTypesDal _messageTypesDal){
+        this.messageTypesDal=_messageTypesDal;
     }
 
-    public List<MEssageTypes> getAllMessageTypes(){
-        return messageTypes.Dal.findAll();
+    public List<MessageTypes> getAllMessageTypes(){
+        return messageTypesDal.findAll();
     }
 
-    public MessageType getMessageTypesId(short id){
+    public MessageTypes getMessageTypesId(short id){
         return messageTypesDal.findById(id);
     }
 
-    public MessageTyps getMessageTypesByName(String name){
+    public MessageTypes getMessageTypesByName(String name){
         if(name==null||name.isBlank()){
             throw new ResourceNotFoundException("Message type name cannot be null or blank.");
         }
--- backend/src/main/java/com/ChatApp/Controller/MessageTypesController.java
+++ backend/src/main/java/com/ChatApp/Controller/MessageTypesController.java
@@ -1,4 +1,21 @@
 package com.ChatApp.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ChatApp.BusinessAccess.MessageTypeBal.MessageTypesBal;
+
+@RestController
+@RequestMapping("/message-types")
 public class MessageTypesController {
+
+    private final MessageTypesBal messageTypesBal;
+
+    public MessageTypesController(MessageTypesBal _messageTypesBal) {
+        this.messageTypesBal = _messageTypesBal;
+    }
+
+
+    
     
 }
