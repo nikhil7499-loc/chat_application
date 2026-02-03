96a0f7b64a228d9cf064b31063905b6c3a29a335
nikhilgaikwad <nikhilgaikwadmh10@gmail.com>
2026-02-03 13:33:02 +0530

    changes

--- backend/src/main/java/com/ChatApp/Entities/ChatGroups.java
+++ backend/src/main/java/com/ChatApp/Entities/ChatGroups.java
@@ -1,10 +1,16 @@
 package com.ChatApp.Entities;
 
+import java.util.UUID;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 
 @Entity
@@ -12,8 +18,8 @@ import jakarta.persistence.Table;
 public class ChatGroups{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name="id",nullable=false,length=36)
-    private Short i;
+    @Column(name="id",nullable=false,length=36,updatable=false)
+    private Short id;
 
     @Column(nullable=false,length=255)
     private String name;
@@ -21,5 +27,118 @@ public class ChatGroups{
     @Column(name="description",length=500)
     private String description;
 
+    @ManyToOne(fetch= FetchType.LAZY)
+    @JoinColumn(name="created_by",nullable=false,)
+    private User createdBy;
+
+    @Column(name="created_at",nullable=false,columnDefinition="timestamp")
+    private Instant createdAt;
+    @Column(name="updated_at",nullable=false,columnDefination="timestamp")
+    private Instant updatedAt;
     
+    
+    @PrePersist
+    protected void oncreate(){
+        if(this.id==null){
+            this.id=UUID.randomUUID().toString();
+        }
+        Instant nowUtc=Instant.now();
+        this.createdAt=nowUtc;
+        this.updatedAt=nowUtc;
+
+    }
+
+    @PrePersist
+    protected void onUpdate(){
+        this.updatedupdatedAt=Instant.now();
+    }
+
+    
+
+
+
+    /**
+     * @return Short return the id
+     */
+    public Short getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Short id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return String return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return User return the createdBy
+     */
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    /**
+     * @param createdBy the createdBy to set
+     */
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    /**
+     * @return Instant return the createdAt
+     */
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    /**
+     * @param createdAt the createdAt to set
+     */
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    /**
+     * @return Instant return the updatedAt
+     */
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    /**
+     * @param updatedAt the updatedAt to set
+     */
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
 }
--- backend/src/main/java/com/ChatApp/Entities/KnownConnection.java
+++ backend/src/main/java/com/ChatApp/Entities/KnownConnection.java
@@ -1,23 +1,84 @@
 
+import com.ChatApp.Entities.User;
+
+import java.time.Instant;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 
 @Entity
-@Table(name="known_connection")
+@Table(name="known_connection",
+    uniqueConstraints={
+        @UniqueConstraint(columnNames={
+            "user_id","contact_id"
+        })
+    }
+)
 public class KnownConnection {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(nullable=false,updatable=false)
-    private Short id;
+    private String id; 
 
-    @Column(nullable=false,updatable=true,length=256)
-    private String name;
+    @ManyToOne(fetch=FetchType.LAZY,optional=false)
+    @JoinColumn(name="user_id",nullable=false)
+    private User user;
 
-    @Column(length=256)
-    private String description;
+    @ManyToOne(fetch=FetchType.LAZY,optional=false)
+    @JoinColumn(name="contact_id",nullable=false)
+    private User contact;
+
+    @Column(name="last_message_at",nullable=false)
+    private Instant lastMessageAt;
+
+    @Column(name="is_blocked",nullable=false)
+    private Boolean  isBlocked=false;
+
+    @Column(name="blocked_by_user_id")
+    private String blockedByuserId;
+
+    @Column(name="blocked_by_user_id")
+    private Instant blockedByUser;
+
+    @Column(name="is_faavorite",nullable=false)
+    private Boolean isFavorite=false;
+    
+    @Column(name="unread_count",nullable=false)
+    private Integer unreadCount=0;
+
+    public KnownConnection(){};
+
+    public KnownConnection(User user, User contact) {
+        this.user = user;
+        this.contact = contact;
+        this.lastMessageAt = Instant.now();
+    }
+
+    @PrePersist
+    public void prePersist(){
+
+        if(lastMessageAt==null){
+            lastMessageAt=Instant.now();
+        }
+
+       if (isFavorite == null) {
+            isFavorite = false;
+        }
+        if (unreadCount == null) {
+            unreadCount = 0;
+        }
+        if (isBlocked == null) {
+            isBlocked = false;
+        }
+    }
 }
