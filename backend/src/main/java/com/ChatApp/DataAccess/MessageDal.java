505e8cb340051b1799fbce52865bf60206ba60c8
nikhilgaikwad <nikhilgaikwadmh10@gmail.com>
2026-02-25 09:35:01 +0530

    lombok

--- backend/src/main/java/com/ChatApp/DataAccess/MessageDal.java
+++ backend/src/main/java/com/ChatApp/DataAccess/MessageDal.java
@@ -1,10 +1,19 @@
 package com.ChatApp.DataAccess
 
+import org.springframework.stereotype.Component;
+
 import com.ChatApp.Repository.MessageRepository;
+
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
+@Component
+@Transactional
 public class MessageDal {
     private final MessageRepository messageRepository;
+
+    publlic MessageDal(MessageRepository messageRepository){
+        this.messageRepository=messageRepository;
+    }
     
 }
